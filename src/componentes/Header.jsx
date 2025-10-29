d776ff3afc85db67877ce3a397e810acbbac092c
ssarmor0101 <ssarmor0101@g.educaand.es>
2025-10-29 16:56:51 +0100

    Agregado etiqueta P

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -2,6 +2,7 @@ function Header() {
     return (
         <header className="header">
             <h1>Titulo de las pizzas (dev1)</h1>
+            <p>Nuevo contenido (dev1)</p>
         </header>
     );
 }
