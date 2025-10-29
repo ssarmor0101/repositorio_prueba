7f6405a14a6feb5e16cae92c3ff9688191dd13a0
dev2 <rlopdav392@g.educaand.es>
2025-10-29 17:11:14 +0100

    movidas footer

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -1,26 +1,27 @@
 import Order from "./Order";
 
 function Footer() {
-    const openHour = "10";
-    const closeHour = "19";
-    const currentHour = new Date().getHours();
-    const isOpen = currentHour >= openHour && currentHour < closeHour;
-    console.log(isOpen);
+  console.log("movidas dev2");
+  const openHour = "10";
+  const closeHour = "19";
+  const currentHour = new Date().getHours();
+  const isOpen = currentHour >= openHour && currentHour < closeHour;
+  console.log(isOpen);
 
-    const objectOrder = { openHour, closeHour};
-    return (
-        <footer className="footer">
-            <div>
-                <p>Soy el footer</p>
-                {isOpen && (
-                    <Order openHour={openHour} closeHour={closeHour}/>
-                    // <Order order={objectOrder}/>
-                )}
-                {/* {isOpen ? <p>Esta abierto.</p> : <p>Esta cerrado.</p>} */}
-                {/* <button className="btn">Soy un boton</button> */}
-            </div>
-        </footer>
-    );
+  const objectOrder = { openHour, closeHour };
+  return (
+    <footer className="footer">
+      <div>
+        <p>Soy el footer</p>
+        {isOpen && (
+          <Order openHour={openHour} closeHour={closeHour} />
+          // <Order order={objectOrder}/>
+        )}
+        {/* {isOpen ? <p>Esta abierto.</p> : <p>Esta cerrado.</p>} */}
+        {/* <button className="btn">Soy un boton</button> */}
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <header className="header">
       <h1>Movida Pizza</h1>
-      <p>developer 2 haciendo movidas en header</p>
+      <p>haciend</p>
     </header>
   );
 }
