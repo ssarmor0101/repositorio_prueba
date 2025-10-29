aa4e2c03aff4f50a320558ae0f94e38c5877552b
dev2 <rlopdav392@g.educaand.es>
2025-10-29 16:56:32 +0100

    cambios en header

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,9 +1,10 @@
 function Header() {
-    return (
-        <header className="header">
-            <h1>Movida Pizza</h1>
-        </header>
-    );
+  return (
+    <header className="header">
+      <h1>Movida Pizza</h1>
+      <p>developer 2 haciendo movidas en header</p>
+    </header>
+  );
 }
 
 export default Header;
